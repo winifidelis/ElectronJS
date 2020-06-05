5b517dbcc24cf04eec67ab1b90b72ca2f5550292
winifidelis <winifidelis@gmail.com>
2020-06-05 01:21:45 -0300

    no message

---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- fluid é para o componente ocupar a tela inteira !-->
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" v-bind:key="word.name"
+      :name="word.name" :amount="word.amount"/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill";
+
+export default {
+  components: { Pill },
+  data: function() {
+    return {
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
